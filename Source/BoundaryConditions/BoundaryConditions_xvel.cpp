92cfe74b771db74552abdfa8c87de2c0944acb8c
Mahesh Natarajan <mahesh@dogora.dhcp.lbl.gov>
2024-02-26 00:09:49 -0800

    Changes to reproduce squall line 2D

--- Source/BoundaryConditions/BoundaryConditions_xvel.cpp
+++ Source/BoundaryConditions/BoundaryConditions_xvel.cpp
@@ -74,7 +74,7 @@ void ERFPhysBCFunct::impose_lateral_xvel_bcs (const Array4<Real>& dest_arr,
                 } else if (bc_ptr[n].lo(0) == ERFBCType::reflect_odd) {
                     dest_arr(i,j,k) = -dest_arr(iflip,j,k);
                 } else if (bc_ptr[n].lo(0) == ERFBCType::neumann_int) {
-                    dest_arr(i,j,k) = (4.0*dest_arr(dom_lo.x+1,j,k) - dest_arr(dom_lo.x+2,j,k))/3.0;
+                    //dest_arr(i,j,k) = (4.0*dest_arr(dom_lo.x+1,j,k) - dest_arr(dom_lo.x+2,j,k))/3.0;
                 }
             },
             // We only set the values on the domain faces themselves if EXT_DIR
@@ -83,7 +83,7 @@ void ERFPhysBCFunct::impose_lateral_xvel_bcs (const Array4<Real>& dest_arr,
               if (bc_ptr[n].lo(0) == ERFBCType::ext_dir) {
                     dest_arr(i,j,k) = l_bc_extdir_vals_d[n][0];
               } else if (bc_ptr[n].lo(0) == ERFBCType::neumann_int) {
-                    dest_arr(i,j,k) = (4.0*dest_arr(dom_lo.x+1,j,k) - dest_arr(dom_lo.x+2,j,k))/3.0;
+                    //dest_arr(i,j,k) = (4.0*dest_arr(dom_lo.x+1,j,k) - dest_arr(dom_lo.x+2,j,k))/3.0;
               }
             }
         );
@@ -100,7 +100,7 @@ void ERFPhysBCFunct::impose_lateral_xvel_bcs (const Array4<Real>& dest_arr,
                 } else if (bc_ptr[n].hi(0) == ERFBCType::reflect_odd) {
                     dest_arr(i,j,k) = -dest_arr(iflip,j,k);
                 } else if (bc_ptr[n].hi(0) == ERFBCType::neumann_int) {
-                    dest_arr(i,j,k) = (4.0*dest_arr(dom_hi.x,j,k) - dest_arr(dom_hi.x-1,j,k))/3.0;
+                    //dest_arr(i,j,k) = (4.0*dest_arr(dom_hi.x,j,k) - dest_arr(dom_hi.x-1,j,k))/3.0;
                 }
             },
             // We only set the values on the domain faces themselves if EXT_DIR
@@ -109,7 +109,7 @@ void ERFPhysBCFunct::impose_lateral_xvel_bcs (const Array4<Real>& dest_arr,
                 if (bc_ptr[n].hi(0) == ERFBCType::ext_dir) {
                     dest_arr(i,j,k) = l_bc_extdir_vals_d[n][3];
                 } else if (bc_ptr[n].hi(0) == ERFBCType::neumann_int) {
-                    dest_arr(i,j,k) = (4.0*dest_arr(dom_hi.x,j,k) - dest_arr(dom_hi.x-1,j,k))/3.0;
+                    //dest_arr(i,j,k) = (4.0*dest_arr(dom_hi.x,j,k) - dest_arr(dom_hi.x-1,j,k))/3.0;
                 }
             }
         );
